03cdbf57994f2e2911c0c2b957de18f33358de90
tzur.mantzur <tzurmm@gmail.com>
2022-08-24 17:24:07 +0300

    upload

---
+++ c/WS2_Pointers_Part 1/pointers_1.c
@@ -0,0 +1,18 @@
+/* date: 24.8
+writer: Tzur
+review: Mark
+Status: approved
+*/
+
+//swap function between two pointers
+ 
+void Swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+
+
