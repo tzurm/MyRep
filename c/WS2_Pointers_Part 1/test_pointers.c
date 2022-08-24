e16025276700044ca35c405c90a6b1845e735f53
tzur.mantzur <tzurmm@gmail.com>
2022-08-24 16:04:11 +0300

    upload

--- c/WS2_Pointers_Part 1/test_pointers.c
+++ c/WS2_Pointers_Part 1/test_pointers.c
@@ -1,26 +1,20 @@
-
 #include <stdio.h>
 #include "all_functions.h"
 #include "all_functions.c"
 
 
-
 int main() 
 {
-   
     int num1,num2;
-    char hex[]={0x22,0x48,0x65,0x6c,0x6c,0x6f,0x20,0x57,0x6f,0x72,0x6c,0x64,0x21,0x22,'\0'};
-  
-    printf("Enter num to start function\n1:\"Hello World!\" \n2: pow \n3: flip \n4: swap\n");
     printf("Enter num for num1 ");
-    scanf("%d",&xnum1;
+    scanf("%d",&num1);
     printf("Enter num for num2 ");
     scanf("%d",&num2);
     printf("The values before swapping: \n");
-    printf("x: %d  y: %d \n", x, y);
-    Swap(&x,&y);
+    printf("num1: %d  num2: %d \n", num1, num2);
+    Swap(&num1,&num2);
     printf("The values after swapping: \n");
-    printf("x: %d  y: %d \n", x, y);
+    printf("num1: %d  num2: %d \n", num1, num2);
     
-    }
     return 0;
+  }
