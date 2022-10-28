1cbdc9074f9c0770908edbc9bcbe1f42298db6df
tzur.mantzur <tzurmm@gmail.com>
2022-10-28 12:19:25 +0300

    upload

--- system_programming/vsa/vsa.c
+++ system_programming/vsa/vsa.c
@@ -1,48 +1,47 @@
 #include <assert.h> /*	assert	*/
 #include <stddef.h> /*	size_t	*/
 #include <stdlib.h> /*	malloc	*/
-#include <stdio.h>	/*	printf	*/
 #include "vsa.h"	/*	fsa		*/
 
 #define WORD sizeof(vsa_t)
 #define DUMMY 1
 #define END (long)0xBADC0FFEE0DDF00D
 #define MARK_DEBUG (long)0xDEADBABEBAAAAAAD
-#define BLOCK_SIZE(ptr) ((ptr -> block_size))
-
+#define BLOCK_SIZE(ptr) ((ptr->block_size))
+#define ROUND_UP(size) ((size + WORD - 1) & ~(WORD - 1))
+#define ROUND_DOWN(size) ((size) & (-WORD))
 struct vsa
 {
 	long block_size;
-/*		
-	#ifndef NDEBUD
-		size_t debug_num;
-	#endif
-*/	
+	/*
+		#ifndef NDEBUD
+			size_t debug_num;
+		#endif
+	*/
 };
 
 static size_t AlignedSize(size_t size)
-{	
-	/* round up */
-	return (size + WORD-1) & ~(WORD-1);
+{
+	return ROUND_UP(size);
 }
 
 /****************************		DEFRAG		*****************************
  *																			*
  *			p1			p2													*
- *		+----------+----+------+---+------------+-----+						*
+ *		+---+------+----+------+---+------------+-----+						*
  *		| 8 | DATA | 24 | DATA |-16|			| END |						*
- *		+----------+----+------+---+------------+-----+						*
+ *		+---+------+----+------+---+------------+-----+						*
  *		0  0x8	 0x16  0x24  0x48  0x56			0x72						*
  *																			*
  * 		free(p1) , free(p2)													*
  *																			*
  *		+----+-----------------+---+------------+-----+						*
  *		| 40 | 				   |-16|			| END |						*
- *		+----------------------+---+------------+-----+						*
+ *		+----+-----------------+---+------------+-----+						*
  *		0	0x8				 0x48  0x56			0x72						*
  *																			*
  ****************************************************************************/
-static void Defrag(void *mem)
+static void *Defrag(void *mem)
 {
 	vsa_t *save_before_defrag = NULL;
 	vsa_t *runner = NULL;
@@ -63,19 +62,28 @@ static void Defrag(void *mem)
 			BLOCK_SIZE(save_before_defrag) = sum;
 		}
 	}
+	return save_before_defrag;
 }
-
+/*-----------------------------Init-----------------------------------------*
+ *		pool 201 -> 200 -8 -8 ->184											*														*
+ *																			*
+ *		+-----+---------------------------------+-----+						*
+ *		| 184 |	  								| END |						*
+ *		+-----+---------------------------------+-----+						*
+ *		0	 0x8							  0x192   0x200					*
+ *																			*
+ *--------------------------------------------------------------------------*/
 vsa_t *Init(size_t pool_size, void *mem)
 {
 	vsa_t *runner = NULL;
-	
+
 	assert(NULL != mem);
-	
+
 	runner = (vsa_t *)mem;
-	pool_size = AlignedSize(pool_size) - 2 * WORD;
-	
-	BLOCK_SIZE(runner) = pool_size; /* size pool left*/
-	runner += pool_size / WORD;
+	pool_size = ROUND_DOWN(pool_size) - 2 * WORD;
+
+	BLOCK_SIZE(runner) = pool_size;	  /*set the first block to pool size*/
+	runner += (pool_size / WORD) + 1; /*jump to end and set the magin num */
 	BLOCK_SIZE(runner) = END;
 
 	return (vsa_t *)mem;
@@ -84,83 +92,78 @@ vsa_t *Init(size_t pool_size, void *mem)
 /****************************************************************************
  * sent pool: 85 -> aligned to 80 - two words -> 64							*
  *																			*
- *		+---------------------------------------------+						*
- *		| 64 |									| END *		  				*
- *		+---------------------------------------------+						*
- *		0  0x8								  x72							*
- *																			*
- * 		aloc 8																*
- *					64-8-8		 											*
  *		+----+----------------------------------+-----+						*
+ *		| 64 |									| END *		  				*
+ *		+----+----------------------------------+-----+						*
+ *		0  0x8								  x72							*
+ *																			*														*
+ *		 __aloc 8__															*
+ * 		/		   \ 64-8-8		 											*
+ *		+----+------+----+----------------------+-----+						*
  *		| -8 | DATA | 48 |					    | END *						*
- *		+---------------------------------------+-----+						*
+ *		+----+------+----+----------------------+-----+						*
  *		0	0x8	  0x16  0x24 				   0x72							*
  *																			*
- * 		aloc 24					 48-24-8									*
- *		+----+----------------------------------+-----+						*
+ * 					  _aloc 24__											*
+ *					 /		    \ 48-24-8									*
+ *		+----+------+-----+------+----+---------+-----+						*
  *		| -8 | DATA | -24 | DATA | 16 |		    | END *						*
- *		+---------------------------------------+-----+						*
+ *		+----+------+-----+------+----+---------+-----+						*
  *		0	0x8	  0x16  0x24  0x48 0x56		   0x72							*
  *																			*
  ****************************************************************************/
 
-/********************************************************
- * pseudo												*
- * keep run until reach to end							*
- * 		if you see marked blocked						*
- * 			step by his absolute value					*
- * 		if you see size smaller than you				*
- *   		check the next one if free do defrag		*
- * 		if you see size bigger than you					*
- * 			mark the current block to minus you			*
- * 			set the next block to left mem (pool - you)	*
- ********************************************************/
+/****************************************************************************
+ * pseudo																	*
+ * run until reach to end or unmarked block	by step by his absolute value	*					*
+ * if you reached to end return NULL										*
+ * if current block is too small perform defrag it possible					*
+ * mark the new block (by minus value)										*				*
+ * set the next block to left mem (before - new block - size of word)		*
+ * 																			*
+ ****************************************************************************/
 void *Alloc(vsa_t *pool, size_t required_size)
 {
 	vsa_t *runner = NULL;
 	vsa_t *save_runner = NULL;
 	long temp = 0;
 	long skip = 0;
-	
+
 	assert(NULL != pool);
-	
+
 	runner = pool;
 	required_size = AlignedSize(required_size);
 
-	/* run until reach to end , if this block marked keep going */
+	/* run until reach to end or unmarked block */
 	while (END != BLOCK_SIZE(runner) && BLOCK_SIZE(runner) < 0)
 	{
 		skip = -BLOCK_SIZE(runner);
 		runner = (vsa_t *)((char *)runner + sizeof(vsa_t) + skip);
 	}
-	/* if get to end return NULL*/
+	/* if reached to end return NULL*/
 	if (END == BLOCK_SIZE(runner))
 	{
 		return NULL;
 	}
-	/* when you have space*/
+	/* if current block is too small perform defrag when possible*/
 	if ((long)required_size > BLOCK_SIZE(runner))
 	{
-		printf("defrag\n");
 		Defrag(runner);
 	}
-	else
-	{
-		temp = BLOCK_SIZE(runner);
-		BLOCK_SIZE(runner) = -required_size;
-		save_runner = (vsa_t *)((char *)runner);
-		runner = (vsa_t *)((char *)runner + required_size + sizeof(vsa_t));
-		BLOCK_SIZE(runner) = temp - required_size - sizeof(vsa_t);
-	/*	
+
+	temp = BLOCK_SIZE(runner);
+	BLOCK_SIZE(runner) = -required_size;
+	save_runner = (vsa_t *)((char *)runner);
+	runner = (vsa_t *)((char *)runner + required_size + sizeof(vsa_t));
+	BLOCK_SIZE(runner) = temp - required_size - sizeof(vsa_t);
+	/*
 		#ifndef NDEBUD
 			save_runner->debug_num = MARK_DEBUG;
 		#endif
 	*/
-	}
-	
+
 	save_runner = (vsa_t *)((char *)save_runner + sizeof(vsa_t));
-	
-	
+
 	return (vsa_t *)save_runner;
 }
 
@@ -172,16 +175,15 @@ void Free(void *mem)
 {
 	vsa_t *runner = NULL;
 	assert(NULL != mem);
-/*	
-	#ifndef NDEBUD
-		assert(MARK_DEBUG == (runner -> debug_num));
-		runner -> debug_num = 0;
-	#endif
-*/	
+	/*
+		#ifndef NDEBUD
+			assert(MARK_DEBUG == (runner -> debug_num));
+			runner -> debug_num = 0;
+		#endif
+	*/
 	runner = (vsa_t *)mem;
 	runner = (vsa_t *)((char *)runner - sizeof(vsa_t));
 	BLOCK_SIZE(runner) = -1 * (BLOCK_SIZE(runner));
-	printf("clean	%ld bytes\n", BLOCK_SIZE(runner));
 }
 
 /********************************************************
--- system_programming/vsa/vsa_test .c
+++ system_programming/vsa/vsa_test .c
@@ -3,13 +3,31 @@
 #include <stdlib.h> /*		malloc		*/
 #include "vsa.h"	/*		fsa			*/
 
+#define END (long)0xBADC0FFEE0DDF00D
+
+static void PrintAlloc(void* mem)
+{
+	long *runner =NULL;
+	runner =(long*)mem;
+
+	while(END != *runner) 
+    {
+        printf("%p	| %ld\n",runner,*runner);
+        runner += 1;
+    }
+	 printf("%p	| %ld\n",runner,*runner);
+}
+
 void Test_Init();
+void Test_Init1();
 void Test_Defrag();
 int main()
 {
-
 	printf("*************************\n*   Test_Init		* \n*************************\n");
 	Test_Init();
+
+	printf("*************************\n*   Test_Init1		* \n*************************\n");
+	Test_Init1();
 
 	printf("*************************\n*   Test_Defrag		* \n*************************\n");
 	Test_Defrag();
@@ -19,37 +37,48 @@ int main()
 
 void Test_Init()
 {
+	void *mem = NULL;
+	vsa_t *pool = NULL;
+	size_t *runner =NULL;
+	size_t pool_size = 32;
+
+	mem = malloc(pool_size);
+	pool = Init(pool_size, mem);
+	runner =(size_t*)pool;
+	printf("pool size:	%ld\n", *runner);
+	PrintAlloc(mem);
+	free(mem);
+
+}
+void Test_Init1()
+{
 
 	void *mem = NULL;
 	vsa_t *pool = NULL;
-	size_t pool_size = 200;
+	size_t pool_size = 201;
 	void *p1 = NULL, *p2 = NULL, *p3 = NULL, *p4 = NULL, *p5 = NULL;
-
-	size_t i = 0;
 	size_t *runner =NULL;
 
 	mem = malloc(pool_size);
 	pool = Init(pool_size, mem);
 	runner =(size_t*)pool;
-
+	printf("pool size:	%ld\n", *runner);
+	PrintAlloc(mem);
+/*
 	printf("Largest: %ld\n" ,LargestChunkAvailable(pool));
-	
+*/	
 	p1 = Alloc(pool, 7);  
 	p2 = Alloc(pool, 20); 
 	p3 = Alloc(pool, 10); 
-	
+/*
 	printf("Largest: %ld\n" ,LargestChunkAvailable(pool));
+*/
 	p4 = Alloc(pool, 40); 
-	while(24 > i) 
-    {
-        printf("runner:	%ld\n",*runner);
-        runner += 1;
-        ++i;
-    }
+	PrintAlloc(mem);
+/*
 	printf("Largest: %ld\n" ,LargestChunkAvailable(pool));
+*/
 	p5 = Alloc(pool, 40); 
-
-	i = 0;
 	runner =(size_t*)pool;
 	Free(p1);
 	Free(p2);
@@ -57,12 +86,7 @@ void Test_Init()
 	Free(p4);
 	Free(p5);
 	
-	while(24 > i) 
-    {
-        printf("runner:	%ld \n",*runner);
-        runner += 1;
-        ++i;
-    }
+	PrintAlloc(mem);
 	free(mem);
 }
 void Test_Defrag()
@@ -71,22 +95,19 @@ void Test_Defrag()
 	vsa_t *pool = NULL;
 	size_t pool_size = 85;
 	void *p1 = NULL, *p2 = NULL, *p3 = NULL;
-	size_t i = 0;
 	size_t *runner =NULL;
 
 	mem = malloc(pool_size);
 	pool = Init(pool_size, mem);
 	runner =(size_t*)pool;
-
+	printf("pool size:	%ld\n", *runner);
 	p1 = Alloc(pool, 7);  
 	p2 = Alloc(pool, 8); 
 	p3 = Alloc(pool, 8); 
-	/*free(p3);*/
-	while(10 > i) 
-    {
-        printf("runner:	%ld\n",*runner);
-        runner += 1;
-        ++i;
-    }
+	PrintAlloc(mem);
+	Free(p1);
+	Free(p2);
+	Free(p3);
+	free(mem);
 }
 
