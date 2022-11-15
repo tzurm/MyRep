359fc9f8633da601810f911b08d0dc2766dc3c72
tzur.mantzur <tzurmm@gmail.com>
2022-11-15 18:14:11 +0200

    new_patch

--- ds/source/hash/hash_test.c
+++ ds/source/hash/hash_test.c
@@ -5,32 +5,32 @@
 #include "sll.h"
 #include "hash.h"
 
-#define TABLE_SIZE 10
+#define TABLE_SIZE 30
 
 int CompareFunc(const void *data, void *param);
 size_t HashFunc(void *data);
 void CountCollision(size_t *arr, size_t len);
 
-void Test_Only_Create(void);
 void Test_Create(void);
+void Test_Insert_Size_t(void);
 void Test_HashFunc(void);
-void Test_Insert(void);
+void Test_Insert_words(void);
 
 int main()
 {
-	printf("\n---------Test_Only_Create-------------------------------\n");
-	Test_Only_Create();
 	printf("\n---------Test_Create-------------------------------\n");
 	Test_Create();
+	printf("\n---------Test_Insert_Size_t-------------------------------\n");
+	Test_Insert_Size_t();
 	printf("\n---------Test_HashFunc-----------------------------\n");
 	Test_HashFunc();
-	printf("\n---------Test_Insert-----------------------------\n");
-	Test_Insert();
+	printf("\n---------Test_Insert_words-----------------------------\n");
+	Test_Insert_words();
 
 	return 0;
 }
 
-void Test_Only_Create(void)
+void Test_Create(void)
 {
 	hash_t *table = NULL;
 	size_t table_size = 10;
@@ -39,13 +39,13 @@ void Test_Only_Create(void)
 	(NULL == table) ? printf("table is null") : printf("not null");
 }
 
-void Test_Create(void)
+void Test_Insert_Size_t(void)
 {
 	size_t arr[] = {25, 15, 50, 10, 786745, 35, 4, 12, 18, 24, 70, 987797, 44, 66, 90};
 	size_t len = sizeof(arr) / sizeof(arr[0]);
 	size_t i = 0;
 	hash_t *table = NULL;
-	size_t table_size = 10;
+	size_t table_size = TABLE_SIZE;
 	(void)table;
 	table = Create(table_size, CompareFunc, HashFunc);
 
@@ -54,41 +54,35 @@ void Test_Create(void)
 		Insert(table, &arr[i]);
 	}
 }
-void Test_HashFunc(void)
+void Test_Insert_words(void)
 {
-	size_t arr[] = {25, 15, 50, 10, 786745, 35, 4, 12, 18, 24, 70, 987797, 44, 66, 90};
-	size_t len = sizeof(arr) / sizeof(arr[0]);
-	size_t arr_hash[50] = {0};
-	size_t i = 0;
-
-	printf("index\tvalue\thash-value\n");
-	for (i = 0; i < len; ++i)
-	{
-		arr_hash[i] = HashFunc(&arr[i]);
-		printf("%ld\t%ld\t%ld\n", i, arr[i], HashFunc(&arr[i]));
-	}
-	printf("Collision-------------\n");
-	CountCollision(arr_hash, len);
-}
-void Test_Insert(void)
-{
+	hash_t *table = NULL;
+	size_t table_size = TABLE_SIZE;
 	FILE *fp = fp;
 	char str[60] = "";
 	size_t i = 0;
+	size_t arr_hash[TABLE_SIZE] = {0};
+
+	table = Create(table_size, CompareFunc, HashFunc);
 
 	fp = fopen("words.txt", "r");
 	if (NULL == fp)
 	{
 		perror("Error opening file");
 	}
-	for (i = 0; i < 10; ++i)
+	printf("index\tvalue\t\t\t\thash-value\n");
+	printf("-------------------------------------------------------\n");
+	for (i = 0; i < table_size; ++i)
 	{
 		if (NULL != fgets(str + i, 60, fp))
 		{
-			puts(str);
+			arr_hash[i] = HashFunc(&str);
+			printf("%ld\t%s\t\t\t\t\t%ld\n", i, str, HashFunc(&arr_hash[i]));
+			Insert(table, &str);
 		}
 	}
 	fclose(fp);
+	CountCollision(arr_hash, TABLE_SIZE);
 }
 /******************************************************************/
 int CompareFunc(const void *data, void *param)
@@ -128,9 +122,26 @@ void CountCollision(size_t *arr, size_t len)
 			if (arr[i] == arr[j])
 			{
 				++count;
-				printf("%ld , %ld \n", i, j);
+				printf("indexes:\t%ld , %ld \n", i, j);
 			}
 		}
 	}
-	printf("Total %ld\n", count);
+	printf("Total\t\t%ld\n", count);
+}
+
+void Test_HashFunc(void)
+{
+	size_t arr[] = {25, 15, 50, 10, 786745, 35, 4, 12, 18, 24, 70, 987797, 44, 66, 90};
+	size_t len = sizeof(arr) / sizeof(arr[0]);
+	size_t arr_hash[50] = {0};
+	size_t i = 0;
+
+	printf("index\tvalue\thash-value\n");
+	for (i = 0; i < len; ++i)
+	{
+		arr_hash[i] = HashFunc(&arr[i]);
+		printf("%ld\t%ld\t%ld\n", i, arr[i], HashFunc(&arr[i]));
+	}
+	printf("Collision-------------\n");
+	CountCollision(arr_hash, len);
 }
