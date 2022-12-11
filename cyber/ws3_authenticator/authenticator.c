7975887affcefb6ea5f37db19ca6a39b6f83a524
tzur.mantzur <tzurmm@gmail.com>
2022-12-11 16:45:17 +0200

    up

---
+++ cyber/ws3_authenticator/authenticator.c
@@ -0,0 +1,141 @@
+
+#include <stdio.h>  /*function file*/
+#include <string.h> /*strlen*/
+#include <assert.h> /*assert*/
+#include <crypt.h>  /*crypt*/
+#include "authenticator.h"
+
+#define MAX_USERNAME_LEN 32
+#define MAX_PASSWORD_LEN 64
+#define FOUND 1
+#define NOT_FOUND -1
+
+/*
+ * Adds a new user to the user file with the given user name and password.
+ * Returns 1 on success, -1 on failure.
+ */
+
+int AddUser(const char *user_name, const char *password)
+{
+    char line[256] = ""; /*to read the current line and keep scan*/
+    int status = 0;
+    char *hashed_password = "0";
+    FILE *file = NULL;
+
+    assert(strlen(user_name) < MAX_USERNAME_LEN);
+    assert(strlen(password) < MAX_PASSWORD_LEN);
+
+    file = fopen("mydatabase", "a+");
+    if (NULL == file)
+    {
+        return -1;
+    }
+    /*check if the user already exists*/
+    while (NULL != fgets(line, sizeof(line), file))
+    {
+        if (strncmp(line, user_name, strlen(user_name)) == 0)
+        {
+            fclose(file);
+            return -1;
+        }
+    }
+    hashed_password = crypt(password, "spider");
+
+    status = fprintf(file, "%s:%s\n", user_name, hashed_password);
+    if (status < 0)
+    {
+        fclose(file);
+        return -1;
+    }
+
+    fclose(file);
+
+    return 1;
+}
+
+/*
+ * check if the user have authenticate by given user name and password.
+ * Returns 1 on success, -1 on failure.
+ */
+int Authenticate(const char *user_name, const char *password)
+{
+
+    char line[256] = "";        /*to read the current line and keep scan*/
+    char *hashed_password = ""; /*to compare hash to hash*/
+    FILE *file = NULL;
+
+    assert(strlen(user_name) < MAX_USERNAME_LEN);
+    assert(strlen(password) < MAX_PASSWORD_LEN);
+
+    file = fopen("mydatabase", "r");
+    if (NULL == file)
+    {
+        return -1;
+    }
+
+    while (NULL != fgets(line, sizeof(line), file))
+    {
+
+        if (strstr(line, user_name)) /* Check if user name in line */
+        {
+            /* Check if the password matches by find the hahs in line */
+            hashed_password = crypt(password, "spider");
+            if (strstr(line, hashed_password))
+            {
+                fclose(file);
+                return FOUND;
+            }
+        }
+    }
+
+    fclose(file);
+    return NOT_FOUND;
+}
+
+/*
+ * Delet user by password
+ * Returns 1 on success, -1 on failure.
+ */
+int DeleteUser(const char *user_name)
+{
+    FILE *file = NULL;
+    FILE *tmp = NULL;
+    char line[256] = "";
+    int user_found = 0;
+
+    file = fopen("mydatabase", "r");
+    if (NULL == file)
+    {
+        return -1;
+    }
+
+    while (NULL != fgets(line, sizeof(line), file))
+    {
+        if (strstr(line, user_name))
+        {
+            if (strncmp(line, user_name, strlen(user_name)) == 0)
+            {
+                user_found = FOUND;
+                continue; /*start over and check next line*/
+            }
+        }
+
+        /* If the user was not found, write the current line to a temporary file */
+        tmp = fopen("mydatabase.tmp", "a");
+        fputs(line, tmp);
+        fclose(tmp);
+    }
+
+    /* When done to check every line,
+    this tmp file contains only lines without the selected user name */
+
+    fclose(file);
+
+    if (FOUND == user_found)
+    {
+        rename("mydatabase.tmp", "mydatabase");
+        return 1;
+    }
+
+    return -1;
+}
---
+++ cyber/ws3_authenticator/authenticator_test.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void PrintAuthenticateTest(const char *user_name, const char *password);
+int AddUser(const char *user_name, const char *password);
+int Authenticate(const char *user_name, const char *password);
+int DeleteUser(const char *user_name);
+
+int main()
+{
+
+    char *username = "tzur";
+    char *password = "123456";
+    char *notusername = "pop";
+    char *notpass = "12345";
+
+    /*--------------------------------------------------------Test AddUser----*/
+    AddUser(username, password);
+    AddUser("2tzur", "mypass123");
+    AddUser("user0", "mypass123");
+    AddUser("user1", "12345678");
+
+    /*---------------------------------------------------Test Authenticate----*/
+
+    PrintAuthenticateTest(username, password);   /*found*/
+    PrintAuthenticateTest(username, password);   /*found*/
+    PrintAuthenticateTest("tzur", "123456");     /*found*/
+    PrintAuthenticateTest("user0", "mypass123"); /*found*/
+    PrintAuthenticateTest("joo", notpass);       /*not found*/
+    PrintAuthenticateTest(notusername, notpass); /*not found*/
+
+    /*-----------------------------------------------------Test DeleteUser----*/
+
+    printf("delete 'tzur' user and then check authenction\n");
+    DeleteUser("tzur");
+    PrintAuthenticateTest(username, password); /*not found*/
+
+    return 1;
+}
+
+static void PrintAuthenticateTest(const char *user_name, const char *password)
+{
+    (1 == Authenticate(user_name, password)) ? printf("%s verified\n", user_name) : printf("%s not found\n", user_name);
+}
