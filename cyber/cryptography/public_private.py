33b2647e1574244e3eeddf87d44b7db68af8af62
tzur.mantzur <tzurmm@gmail.com>
2023-01-12 15:58:24 +0200

    s

---
+++ cyber/cryptography/public_private.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Alice generates a pair of keys
+p = 11
+q = 3
+n = p*q
+phi = (p-1)*(q-1)
+e = 7
+d = 3
+
+# Bob encrypts the message using Alice's public key
+message = 13
+ciphertext = (message**e) % n
+
+# Bob sends the encrypted message to Alice
+
+# Alice receives the message and decrypts it using her private key
+plaintext = (ciphertext ** d) % n
+
+# Alice can now read the message
+print(plaintext)  # Output: 12
