aca47adb39b13a3269db93ece25178b044de3060
tzur.mantzur <tzurmm@gmail.com>
2023-01-24 09:14:18 +0200

    vpn

---
+++ cyber/tunneling/client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import socket
+import rsa
+
+HOST = "192.168.0.198"  
+PORT = 4567
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    
+    # Receive the public key from the server
+    pubkey = rsa.PublicKey.load_pkcs1(s.recv(1024))
+    
+    file_name = "top_secret.txt"
+    with open(file_name, "rb") as f:
+        file_contents = f.read()
+        # Encrypt the file contents using the public key
+        encrypted_file_contents = rsa.encrypt(file_contents, pubkey)
+        s.sendall(encrypted_file_contents)
+        print(f"File sent: {file_name}")
---
+++ cyber/tunneling/server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import socket
+import rsa
+
+HOST = "192.168.0.198"  
+PORT = 4567
+
+# Generate a public/private key pair
+(pubkey, privkey) = rsa.newkeys(512)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        
+        # Send the public key to the client
+        conn.sendall(pubkey.save_pkcs1())
+        
+        file_name = input("Enter a name for the received file: ")
+        with open(file_name, "wb") as f:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                # Decrypt the data using the private key
+                decrypted_data = rsa.decrypt(data, privkey)
+                f.write(decrypted_data)
+        print(f"File received: {file_name}")
